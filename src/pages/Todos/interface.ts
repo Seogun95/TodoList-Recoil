40defc892a8e12eff276fe37a7795292179f0db1
Seogun95 <ksj083895@gmail.com>
2023-06-12 23:12:42 +0900

    feat: 카테고리 enum 추가

--- src/pages/Todos/interface.ts
+++ src/pages/Todos/interface.ts
@@ -1,7 +1,12 @@
-import { TODO_CATEGORY } from 'utils';
+export enum TODO_CATEGORY {
+  TODO = '진행 예정',
+  DOING = '진행',
+  DONE = '완료',
+  CANCEL = '취소',
+}
 
 export interface ITodoValues {
   todoText: string;
   category: TODO_CATEGORY;
-  id: number;
+  id: string;
 }
