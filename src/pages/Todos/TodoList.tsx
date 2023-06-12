fb67806a01045389d3b0cce3bab58c7c693b3b6a
Seogun95 <ksj083895@gmail.com>
2023-06-12 23:14:23 +0900

    feat: custom category 핸들러 및 조건부 수정

--- src/pages/Todos/TodoList.tsx
+++ src/pages/Todos/TodoList.tsx
@@ -1,28 +1,50 @@
-import { useRecoilValue } from 'recoil';
-import { filteredTodoListState, todoStateAtom } from 'atom';
+import { v4 as uuidv4 } from 'uuid';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, filteredTodoListState, todoStateAtom } from 'atom';
 import { TodoTemplate } from './TodoTemplate';
 import { Todo } from './Todo';
+import { TODO_CATEGORY } from './interface';
 
 export function TodoList() {
-  const todoText = useRecoilValue(todoStateAtom);
-  const [toDos, doing, done] = useRecoilValue(filteredTodoListState);
+  const toDos = useRecoilValue(filteredTodoListState);
+  const todoCategoryArr = Object.values(TODO_CATEGORY);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(e.currentTarget.value as TODO_CATEGORY);
+  };
+
+  const getAllTodo = useRecoilValue(todoStateAtom);
+  let categories = getAllTodo.map(todo => todo.category);
+  let allCategories = todoCategoryArr.concat(categories);
+  let filteredCategories = Array.from(new Set(allCategories)).filter(
+    v => !v.includes('취소')
+  );
 
   return (
     <>
       <h1>TODO LIST</h1>
       <hr />
       <div>
+        <select value={category} onInput={onInput}>
+          {filteredCategories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+
         <TodoTemplate />
+        <h2>{category}</h2>
+        {toDos.length > 0 ? (
+          <ul>
+            {toDos?.map(category => (
+              <Todo key={uuidv4()} {...category} />
+            ))}
+          </ul>
+        ) : (
+          <span>목록이 존재하지 않습니다</span>
+        )}
       </div>
-      <hr />
-      <h2>진행 예정 목록</h2>
-      <ul>{toDos.map(v => <Todo key={v.id} {...v} />).reverse()}</ul>
-      <hr />
-      <h2>진행 중</h2>
-      <ul>{doing.map(v => <Todo key={v.id} {...v} />).reverse()}</ul>
-      <hr />
-      <h2>완료</h2>
-      <ul>{done.map(v => <Todo key={v.id} {...v} />).reverse()}</ul>
       <hr />
     </>
   );
